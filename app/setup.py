ba019480c9ee22bf5485d55e3fe574e2cf105be1
Prateek Kumawat <prateekkumawat20@gmail.com>
2026-01-04 11:34:00 +0530

    add new reelase

--- app/setup.py
+++ app/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="highskyapp",
-    version="4.0.0",
+    version="5.0.0",
     packages=find_packages(),
     install_requires=[
         "requests",
